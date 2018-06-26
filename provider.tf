34a6e18494154268e097f6ccf54736607ced8e64
Dave Arnold <darnold@hashicorp.com>
2018-06-26 16:11:31 -0700

    removing datadog stuff

--- provider.tf
+++ provider.tf
@@ -1,12 +1,7 @@
 # Configure the Azure Provider
-provider "azurerm" { }
+provider "azurerm" {}
 
 provider "consul" {
   address    = "${var.consul_host}"
   datacenter = "${var.consul_cluster}"
-}
-
-provider "datadog" {
-  api_key = "${var.datadog_api_key}"
-  app_key = "${var.datadog_app_key}"
 }
