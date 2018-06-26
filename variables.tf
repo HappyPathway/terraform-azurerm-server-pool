4502f7afed4c0e6b5d1ca529b7b69d853e3c998c
Dave Arnold <darnold@hashicorp.com>
2018-06-26 16:03:33 -0700

    adding all

--- variables.tf
+++ variables.tf
@@ -1,67 +1,56 @@
 variable "location" {
-    default = "West US 2"
+  default = "West US 2"
 }
 
-
 variable "version" {}
+
 variable "service_name" {
-    default = "app"
+  default = "app"
 }
 
 variable "service_port" {
-    default = "80"
+  default = "80"
 }
 
 variable "register_service" {
-    default = true
+  default = true
 }
+
 variable "env" {
-    default = "staging"
+  default = "staging"
 }
 
 variable "count" {
-    default = 1
+  default = 1
 }
 
 variable "disk_size" {
-    default = 1024
+  default = 1024
 }
 
 variable "system_user" {
-    default = "admin"
+  default = "admin"
 }
 
 variable "system_password" {
-    default = "admin"
+  default = "admin"
 }
 
 variable "datadog_api_key" {}
 variable "datadog_app_key" {}
 
 variable "ddog_install_script" {
-    default = "https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh"
+  default = "https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh"
 }
 
 variable "datadog_monitor" {
-    default = true
+  default = true
 }
-
-variable "consul_cluster" {
-    default = "consul-westus"
-} 
-
-variable "consul_host" {
-    default = "localhost:8500"
-}
-
-variable "azure_subscription" {} 
-variable "azure_tenant" {}
-variable "azure_client" {} 
-variable "azure_secret" {}
 
 variable "resource_group" {}
 variable "subnet_name" {}
 variable "network_name" {}
+
 variable "instance_size" {
-    default = "Standard_DS1_v2"
+  default = "Standard_DS1_v2"
 }
