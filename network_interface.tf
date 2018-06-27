f51b0f7aeb30570f37013995cc2562217ff07420
Dave Arnold <darnold@hashicorp.com>
2018-06-26 17:36:22 -0700

    fixing

--- network_interface.tf
+++ network_interface.tf
@@ -1,3 +1,9 @@
+data "azurerm_subnet" "test" {
+  name                 = "${var.subnet}"
+  virtual_network_name = "${var.env}"
+  resource_group_name  = "${var.env}"
+}
+
 resource "azurerm_network_interface" "ani" {
   name                = "${var.service_name}-${var.env}-ani${format("%03d", count.index + 1)}"
   location            = "${var.location}"
@@ -6,7 +12,7 @@ resource "azurerm_network_interface" "ani" {
 
   ip_configuration {
     name                          = "${var.service_name}-ani${format("%03d", count.index + 1)}"
-    subnet_id                     = "${var.subnet}"
+    subnet_id                     = "${data.azurerm_subnet.test.id}"
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = "${element(azurerm_public_ip.api.*.id, count.index)}"
   }
